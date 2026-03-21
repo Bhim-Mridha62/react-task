29721dd09c1bcfbffbf645d11d13b90e944d94cf
Bhim Mridha <140299430+Bhim-Mridha62@users.noreply.github.com>
2026-03-21 11:55:44 +0530

    add yml

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: "/gamyan-product-list",
+  assetPrefix: "/gamyan-product-list/",
 };
 
 export default nextConfig;
